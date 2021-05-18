375c4036a9d8d72fe4936905f48460e65daafe95
Ruan Mello <ruan_mello1@hotmail.com>
2021-05-18 00:21:44 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/tasks/service/listener/APIListener.kt
@@ -0,0 +1,11 @@
+package com.example.tasks.service.listener
+
+import com.example.tasks.service.model.HeaderModel
+
+interface APIListener<T> {
+
+    fun onSuccess(model: T)
+
+    fun onFailure(str: String)
+
+}
